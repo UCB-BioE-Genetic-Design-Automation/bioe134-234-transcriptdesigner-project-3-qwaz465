5c4ed488c95527f6273ecb49b2434a278441b3f3
Jake <qwaz465@hotmail.com>
2024-10-25 17:30:08 -0700

    added codon freq lists

--- genedesign/transcript_designer.py
+++ genedesign/transcript_designer.py
@@ -10,6 +10,7 @@ class TranscriptDesigner:
     def __init__(self):
         self.codonUsage = {}
         self.rbsChooser = None
+        self.codonLists = {}
 
     def initiate(self) -> None:
         """
@@ -28,6 +29,7 @@ class TranscriptDesigner:
                     self.codonUsage[amino_acid] = {}
         
                 self.codonUsage[amino_acid][codon] = float(relative_frequency)
+        self._generate_codon_lists()
 
     def run(self, peptide: str, ignores: set) -> Transcript:
         #TODO implement monte carlo selection for first group, some of this may need to be done in initiate?
@@ -55,6 +57,31 @@ class TranscriptDesigner:
 
         # Return the Transcript object
         return Transcript(selectedRBS, peptide, codons)
+    
+    def _generate_codon_lists(self):
+        #TODO write tests for this to make sure it works
+        """
+        Creates a 100-element list of codons for each amino acid where codons appear proportional to their relative frequency.
+        """
+        for amino_acid, codons in self.codonUsage.items():
+            codon_list = []
+
+            for codon, frequency in codons.items():
+                # Calculate how many times the codon should appear in a 100-element list
+                count = int(round(frequency * 100))
+                codon_list.extend([codon] * count)
+
+            # If the codon list has more than 100 elements (due to rounding), trim it
+            if len(codon_list) > 100:
+                codon_list = codon_list[:100]
+
+            # If the codon list has fewer than 100 elements, pad it with the most frequent codon
+            elif len(codon_list) < 100:
+                most_frequent_codon = max(codons, key=codons.get)  # Find the most frequent codon
+                codon_list.extend([most_frequent_codon] * (100 - len(codon_list)))
+
+            # Store the codon list for the amino acid
+            self.codonLists[amino_acid] = codon_list
 
 if __name__ == "__main__":
     # Example usage of TranscriptDesigner
